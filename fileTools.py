12a9f9f63252c23a89c5e3b38ecdd7a6e9799ee4
hannu-s <hannu123@windowslive.com>
2015-01-22 20:01:00 +0200

    removed useless binary methods. conversion both ways work

--- fileTools.py
+++ fileTools.py
@@ -3,12 +3,6 @@ import struct
 class FileType():
 	"""docstring for FileType"""
 	@staticmethod
-	def is_file_name_bin(fileName):
-		if fileName[-4:].lower() == '.bin':
-			return True
-		return False
-
-	@staticmethod
 	def is_file_name_txt(fileName):
 		if fileName[-4:].lower() == '.txt':
 			return True
@@ -22,45 +16,36 @@ class FileType():
 
 
 class FileReader():
-	encoding = 'ascii'
 	las_chunk_size = 37
 	"""docstring for FileReader"""
 	def read(self, filePath):
-		if FileType.is_file_name_bin(filePath):
-			return self.__read_binary(filePath)
-		elif FileType.is_file_name_txt(filePath):
+		if FileType.is_file_name_txt(filePath):
 			return self.__read_ascii(filePath)
 		elif FileType.is_file_name_las(filePath):
 			return self.__read_las(filePath)
 		else:
-			raise Exception('Unknown file type. File name has to end either as ".txt", ".las" or ".bin"')
+			raise Exception('Unknown file type. File name has to end either as ".txt" or ".las".')
 
-	def read(self, filePath, fileType):
-		if fileType.lower() == 'bin':
-			return self.__read_binary(filePath)
-		elif fileType.lower() == 'txt':
+	def read_as_type(self, filePath, fileType):
+		if (fileType.lower() == 'txt'):
 			return self.__read_ascii(filePath)
-		elif fileType.lower() == 'las':
+		elif (fileType.lower() == 'las'):
 			return self.__read_las(filePath)
 		else:
-			raise Exception('Unknown file type. Supported types are: "txt", "las" and "bin"')
+			raise Exception('Unknown file type. Supported types are: "txt" and "las".')
 		
-	def set_encoding(self, coding):
-		self.encoding = coding
+	def set_las_chunk_size(size):
+		self.las_chunk_size = int(size)
 		
 	def __read_ascii(self, path):
 		data = list()
 		with open(path) as f:
-		    for line in f:
-		        data.append(line)
+			for line in f:
+				line = line.replace('\n','')
+				parts = line.split(' ')
+				data.append(parts)
 		return data
-
-	def __read_binary(self, path):
-		data = list()
-		with open(path, 'rb') as f:
-			data = f.read().decode(self.encoding)
-		return data
-					
+				
 	def __read_las(self, path):
 		data = list()
 		for b in self.__read_las_binary_chunk(path):
@@ -72,61 +57,49 @@ class FileReader():
 			while True:
 				chunk = f.read(self.las_chunk_size)
 				if chunk:
-					value = struct.unpack('fff', data[:12])
-					value += struct.unpack('H', data[12:14])
-					value += struct.unpack('II', data[14:22])
-					value += struct.unpack('BBBBB', data[22:27])
-					value += struct.unpack('H', data[27:29])
-					value += struct.unpack('d', data[29:37])
+					value = struct.unpack('fff', chunk[:12])
+					value += struct.unpack('H', chunk[12:14])
+					value += struct.unpack('II', chunk[14:22])
+					value += struct.unpack('BBBBB', chunk[22:27])
+					value += struct.unpack('H', chunk[27:29])
+					value += struct.unpack('d', chunk[29:37])
 					yield value
 				else:
 					break
 
 class FileWriter():
-	encoding = 'ascii'
 	"""docstring for FileWriter"""
-	def write(self, filePath, data, formatChar = 'B'):
-		if FileType.is_file_name_bin(filePath):
-			self.__write_binary(filePath, data, formatChar)
-		elif FileType.is_file_name_txt(filePath):
+	def write(self, filePath, data):
+		if FileType.is_file_name_txt(filePath):
 			self.__write_ascii(filePath, data)
 		elif FileType.is_file_name_las(filePath):
 			self.__write_las(filePath, data)
 		else:
-			raise Exception('Unkown file type. File name has to end either as ".txt", ".las" or ".bin"')
+			raise Exception('Unkown file type. File name has to end either as ".txt" or ".las".')
 
-	def write(self, filePath, fileType):
-		if fileType.lower() == 'bin':
-			return self.__write_binary(filePath)
-		elif fileType.lower() == 'txt':
-			return self.__write_ascii(filePath)
-		elif fileType.lower() == 'las':
-			return self.__write_las(filePath)
+	def write_as_type(self, filePath, data, fileType):
+		if (fileType.lower() == 'txt'):
+			return self.__write_ascii(filePath, data)
+		elif (fileType.lower() == 'las'):
+			return self.__write_las(filePath, data)
 		else:
-			raise Exception('Unknown file type. Supported types are: "txt", "las" and "bin"')
-		
-	def set_encoding(self, coding):
-		self.encoding = coding
-		
+			raise Exception('Unknown file type. Supported types are: "txt" and "las".')
+			
 	def __write_ascii(self, path, data):
 		with open(path, 'w') as f:
+			print(data)
 			for line in (data):
+				line = str(line).replace(',','')
+				line = line[1:-1] + '\n'
 				f.write(line)
-
-	def __write_binary(self, path, data, fmt):
-		with open(path, 'wb') as f:
-			# B = unsigned char
-			for line in data:
-				for ch in line:
-					b = int.from_bytes(ch.encode(self.encoding), 'little')#https://docs.python.org/3/library/stdtypes.html 				
-					f.write(struct.pack(fmt, b)) # https://docs.python.org/3.4/library/struct.html
 					
 	def __write_las(self, path, data):
 		with open (path, 'wb') as f:
-			st = struct.pack('fff', data[0], data[1], data[2])
-			st += struct.pack('H', data[3])
-			st += struct.pack('II', data[4], data[5])
-			st += struct.pack('BBBBB', data[6], data[7], data[8], data[9], data[10])
-			st += struct.pack('H', data[11])
-			st += struct.pack('d', data[12])	
-			f.write(st)
+			for line in data:
+				st = struct.pack('fff', float(line[0]), float(line[1]), float(line[2]))
+				st += struct.pack('H', int(line[3]))
+				st += struct.pack('II', int(line[4]), int(line[5]))
+				st += struct.pack('BBBBB', int(line[6]), int(line[7]), int(line[8]), int(line[9]), int(line[10]))
+				st += struct.pack('H', int(line[11]))
+				st += struct.pack('d', float(line[12]))
+				f.write(st)
--- main.py
+++ main.py
@@ -1,44 +1,23 @@
-from fileTools import FileReader, FileWriter, CustomEncoder, FileType
+from fileTools import FileReader, FileWriter
 import sys
 
-def main(read_path, write_path, coding = "utf-8", format = "B"):
-	read_path = str(read_path)
-	write_path = str(write_path)
-	coding = str(coding)
-	format = str(format)
-
+def main(read_path, write_path):
 	reader = FileReader()
-	reader.set_encoding(coding)
-
-	data = reader.read(read_path)
-
-	encoder = CustomEncoder()
-	if (FileType.is_file_name_bin(read_path) and FileType.is_file_name_txt(write_path)):
-		data, coding = encoder.custom_to_ascii(data)
-	elif (FileType.is_file_name_txt(read_path) and FileType.is_file_name_bin(write_path)):
-		data, coding = encoder.ascii_to_custom(data)
-	else:
-		print("You are trying to write to same type of file. Aborting.")
-		exit()
 	writer = FileWriter()
-	writer.set_encoding(coding)
-	writer.write(write_path, data, format)
+	
+	data = reader.read(read_path)
+	writer.write(write_path, data)
 
 def print_error_message(msg):
 	print("Error. Invalid startup arguments")
-	print("Example: Main.py ansii_file_to_be_read.txt converted_bin_file.bin")
+	print("Example: Main.py ansii_file_to_be_read.txt converted_bin_file.las")
 	print(msg)
 
 if __name__ == "__main__":
 	if len(sys.argv) <= 2:
 		print_error_message("Not enough arguments") 
-		exit()
 	elif len(sys.argv) == 3:
 		main(sys.argv[1], sys.argv[2])
-	elif len(sys.argv) == 4:
-		main(sys.argv[1], sys.argv[2], sys.argv[3])
-	elif len(sys.argv) == 5:
-		main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 	else:
 		print_error_message("Too many arguments")
-		exit()
+	exit()
